0c36bfa5dfbbb2945938f044a0d46c3c5b54fb43
ankur51206 <32640688+ankur51206@users.noreply.github.com>
2022-07-12 22:30:02 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,16 @@
-output "sps" {
+output "service_principal_name" {
+  value = azuread_service_principal.sp.display_name
+}
+
+output "azuread_application_password_Secret" {
   value     = azuread_application_password.sp.value
   sensitive = true
+}
+
+output "application_id" {
+  value = azuread_application.sp.application_id
+}
+
+output "application_object_id" {
+  value = azuread_application.sp.object_id
 }
